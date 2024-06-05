839a0263660a0d05f51b674add019aba1f0d549f
Noureldin <nurmn3m@gmail.com>
2024-06-05 23:06:41 +0300

    Building CSV-Reader

---
+++ CSV-Reader/src/main.rs
@@ -0,0 +1,21 @@
+use csv;
+use std::{error::Error, path};
+
+fn main(){
+    if let Err(e) = read_from_file("./customers.csv"){
+        eprintln!("{}", e);
+    }
+}
+
+
+fn read_from_file(path: &str) -> Result<(), Box<dyn Error>>{
+
+    let mut reader = csv::Reader::from_path(path)?;
+
+    for result in reader.records(){
+        let record = result?;
+        println!("{:?}", record);
+    }
+    Ok(())
+
+}
